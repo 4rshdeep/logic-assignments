3d5cf27c1be8fd61fd5dfeaf4f9f8fa2b051494e
Arshdeep Singh <arshdeepiitd@gmail.com>
2019-01-20 16:05:12 +0530

    updates

--- a3/testcase.ml
+++ a3/testcase.ml
@@ -1,7 +1,6 @@
-open structure.Assignment3;;
-(* The test cases go here.
-Below is a sample call to wff function *)
-(* FOL formula e.g: loves(romeo, juliet) *)
+open Structure.A3
+(* The test cases go here. Below is a sample call to wff function *)
+(* FOL formula e.g: loves(romeo, juliet)  *)
 let t1 = C "romeo";;
 let t2 = C "juliet";;
 let fml = PRED("loves", [t1; t2]);;
